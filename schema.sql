e4f29459ef764ed8a240380af7e467c88a4b6270
kuroppe <17245737+kuroppe1819@users.noreply.github.com>
2023-01-08 20:39:09 +0900

    change schema.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
-DROP TABLE IF EXISTS tweet_info;
-CREATE TABLE tweet_info (media_key TEXT PRIMARY KEY NOT NULL, tweet_id TEXT NOT NULL, author_id TEXT NOT NULL, created_at TEXT NOT NULL, image_url TEXT NOT NULL, tweet_text TEXT);
+DROP TABLE IF EXISTS tweets;
+CREATE TABLE tweets (media_key TEXT PRIMARY KEY NOT NULL, tweet_id TEXT NOT NULL, author_id TEXT NOT NULL, created_at TEXT NOT NULL, image_url TEXT NOT NULL, tweet_text TEXT);
 
 DROP TABLE IF EXISTS pokemon_name;
-CREATE TABLE pokemon_name (media_key TEXT PRIMARY KEY NOT NULL, tweet_info_id TEXT NOT NULL, name TEXT);
+CREATE TABLE pokemon_name (media_key TEXT PRIMARY KEY NOT NULL, name TEXT NOT NULL);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(author_id TEXT PRIMARY KEY NOT NULL, username TEXT NOT NULL, name TEXT NOT NULL, profile_image_url TEXT);
