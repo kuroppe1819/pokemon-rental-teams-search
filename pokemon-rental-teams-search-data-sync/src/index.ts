580176b15d0edf799731c29a7c164c97bb36cffe
kuroppe <17245737+kuroppe1819@users.noreply.github.com>
2023-01-08 22:27:18 +0900

    insert users data into users table.

--- pokemon-rental-teams-search-data-sync/src/index.ts
+++ pokemon-rental-teams-search-data-sync/src/index.ts
@@ -4,13 +4,21 @@ export interface Env {
   POKEMON_RENTAL_TEAMS_RECENT_SEARCH_API_KEY: string;
 }
 
-type RentalTeams = {
-  mediaKey: string;
-  tweetId: string;
-  authorId: string;
-  createdAt: string;
-  imageUrl: string;
-  text: string | undefined;
+type RentalTeam = {
+  tweet: {
+    mediaKey: string;
+    tweetId: string;
+    authorId: string;
+    createdAt: string;
+    imageUrl: string;
+    text: string | undefined;
+  };
+  user: {
+    authorId: string;
+    username: string;
+    name: string;
+    profileImageUrl: string | undefined;
+  };
 };
 
 export default {
@@ -24,37 +32,65 @@ export default {
       },
     });
     const rentalTeams = await (
-      await res.json<{ rentalTeams: RentalTeams[] }>()
+      await res.json<{ rentalTeams: RentalTeam[] }>()
     ).rentalTeams;
 
     console.log(JSON.stringify(rentalTeams));
     console.log(`Rental teams length: ${rentalTeams.length}`);
 
     for (const rentalTeam of rentalTeams) {
-      const { results } = await env.DB.prepare(
-        "SELECT media_key FROM tweet_info WHERE media_key = ?"
+      // 画像が登録済みか確認する
+      const { results: selectMediaKeyResults } = await env.DB.prepare(
+        "SELECT media_key FROM tweets WHERE media_key = ?"
       )
-        .bind(rentalTeam.mediaKey)
+        .bind(rentalTeam.tweet.mediaKey)
         .all<{ media_key: string }>();
 
-      if (results === undefined) {
-        throw new Error("Error: results is undefined");
+      if (selectMediaKeyResults === undefined) {
+        throw new Error("Error: selectMediaKeyResults is undefined");
       }
 
-      if (results.length > 0) {
+      if (selectMediaKeyResults.length > 0) {
         continue;
       }
 
       await env.DB.prepare(
-        "INSERT INTO tweet_info (media_key, tweet_id, author_id, created_at, image_url, tweet_text) VALUES (?, ?, ?, ?, ?, ?);"
+        "INSERT INTO tweets (media_key, tweet_id, author_id, created_at, image_url, tweet_text) VALUES (?, ?, ?, ?, ?, ?);"
       )
         .bind(
-          rentalTeam.mediaKey,
-          rentalTeam.tweetId,
-          rentalTeam.authorId,
-          rentalTeam.createdAt,
-          rentalTeam.imageUrl,
-          rentalTeam.text ?? null
+          rentalTeam.tweet.mediaKey,
+          rentalTeam.tweet.tweetId,
+          rentalTeam.tweet.authorId,
+          rentalTeam.tweet.createdAt,
+          rentalTeam.tweet.imageUrl,
+          rentalTeam.tweet.text ?? null
+        )
+        .run();
+
+      // ユーザーが登録済みか確認する
+      const { results: selectAuthorIdResults } = await env.DB.prepare(
+        "SELECT author_id FROM users WHERE author_id = ?"
+      )
+        .bind(rentalTeam.user.authorId)
+        .all<{ author_id: string }>();
+
+      if (selectAuthorIdResults === undefined) {
+        throw new Error("Error: selectAuthorIdResults is undefined");
+      }
+
+      if (selectAuthorIdResults.length > 0) {
+        continue;
+      }
+
+      // users テーブルに が登録されていない場合は追加
+      await env.DB.prepare(
+        "INSERT INTO users (author_id, username, name, profile_image_url) VALUES (?, ?, ?, ?);"
+      )
+        .bind(
+          rentalTeam.user.authorId,
+          rentalTeam.user.username,
+          rentalTeam.user.name,
+          rentalTeam.user.profileImageUrl ?? null
         )
         .run();
     }
