0354fbd5e092d745c78f64de2450aef1856b4917
kuroppe <17245737+kuroppe1819@users.noreply.github.com>
2023-01-09 23:19:53 +0900

    render tweets.

--- pokemon-rental-teams-search-app/app/routes/index.tsx
+++ pokemon-rental-teams-search-app/app/routes/index.tsx
@@ -1,31 +1,40 @@
+import type { LoaderArgs } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
+type Tweet = {
+  mediaKey: string;
+  tweetId: string;
+  authorId: string;
+  createdAt: string;
+  imageUrl: string;
+  text: string | undefined;
+  username: string;
+  name: string;
+  profileImageUrl: string | undefined;
+};
+
+export const loader = async ({ context, params }: LoaderArgs) => {
+  const db = context.DB as D1Database;
+  const { results } = await db
+    .prepare(
+      "SELECT * FROM tweets INNER JOIN users ON tweets.author_id = users.author_id;"
+    )
+    .all<Tweet>();
+  const tweets = results ?? [];
+  return json(tweets);
+};
+
 export default function Index() {
+  const tweets = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Pokemon Rental Teams Search</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {tweets.map((tweet, i) => (
+          <li key={i}>{JSON.stringify(tweet)}</li>
+        ))}
       </ul>
     </div>
   );
