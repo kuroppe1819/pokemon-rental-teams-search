568ed5e3c52483a91ed37c6665c15bbdfaba415f
kuroppe <17245737+kuroppe1819@users.noreply.github.com>
2023-01-11 06:04:12 +0900

    change heading color.

--- pokemon-rental-teams-search-app/app/components/Heading.tsx
+++ pokemon-rental-teams-search-app/app/components/Heading.tsx
@@ -1,7 +1,7 @@
 export default function Heading() {
   return (
     <h1 className="text-center mb-4 text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold text-gray-900">
-      <span className="text-transparent bg-clip-text bg-gradient-to-r to-violet-600 from-red-600">
+      <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
         Pokemon Rental Teams Search
       </span>
     </h1>
