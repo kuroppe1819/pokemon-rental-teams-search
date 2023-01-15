7e6c853a63a9b80c3ca5e985221e5d51b50a24a4
kuroppe <17245737+kuroppe1819@users.noreply.github.com>
2023-01-15 12:57:53 +0900

    change db schema.

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS tweets;
 CREATE TABLE tweets (media_key TEXT PRIMARY KEY NOT NULL, tweet_id TEXT NOT NULL, author_id TEXT NOT NULL, created_at TEXT NOT NULL, image_url TEXT NOT NULL, tweet_text TEXT);
 
 DROP TABLE IF EXISTS pokemon_name;
-CREATE TABLE pokemon_name (media_key TEXT PRIMARY KEY NOT NULL, name TEXT NOT NULL);
+CREATE TABLE pokemon_name (id INTEGER PRIMARY KEY AUTOINCREMENT, media_key TEXT NOT NULL, name TEXT NOT NULL);
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(author_id TEXT PRIMARY KEY NOT NULL, username TEXT NOT NULL, name TEXT NOT NULL, profile_image_url TEXT);
