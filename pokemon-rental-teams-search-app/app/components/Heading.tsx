d71b57970f862cd5c93c34195331110b94ff7793
moyashimaru <17245737+kuroppe1819@users.noreply.github.com>
2023-05-07 00:51:20 +0900

    refactor: migrate npm to pnpm.

--- pokemon-rental-teams-search-app/app/components/Heading.tsx
+++ pokemon-rental-teams-search-app/app/components/Heading.tsx
@@ -4,10 +4,8 @@ type Props = {
 
 export default function Heading({ text }: Props) {
   return (
-    <h1 className="text-center mb-4 text-4xl px-6 sm:px-0 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold">
-      <span className="text-transparent bg-clip-text text-cyan-500">
-        {text}
-      </span>
+    <h1 className="text-center text-cyan-500 mb-4 text-4xl px-6 sm:px-0 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold">
+      {text}
     </h1>
   );
 }
