2a15c23b8f4f4025a7ad5119ed6933c91724210c
moyashimaru <17245737+kuroppe1819@users.noreply.github.com>
2023-07-22 21:40:13 +0900

    refactor: generate licenses

--- pokemon-rental-teams-search-app/tools/generate-licenses.js
+++ pokemon-rental-teams-search-app/tools/generate-licenses.js
@@ -2,24 +2,9 @@ const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 
-const stdout = execSync(
-  "npx license-checker-rseidelsohn --production --excludePrivatePackages --json"
-);
-
-const licenses = Object.entries(JSON.parse(stdout)).map(([key, value]) => {
-  const licenseText = fs.readFileSync(value.licenseFile).toString();
-  const text =
-    key +
-    "\n" +
-    `repository: ${value.repository}` +
-    "\n" +
-    `license: ${value.licenses}` +
-    "\n" +
-    licenseText;
-  return text;
-});
+const stdout = execSync("pnpm licenses list --long --prod");
 
 fs.writeFileSync(
   path.join(process.cwd(), "/public/licenses.txt"),
-  licenses.join("\n\n")
+  stdout.toString()
 );
